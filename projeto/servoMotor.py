1414d7c3bf3f249977b0d785b827f396420487eb
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-05-25 18:26:02 -0300

    Update servoMotor.py

--- projeto/servoMotor.py
+++ projeto/servoMotor.py
@@ -1,15 +1,19 @@
-from time import sleep
+from utime import sleep
 from machine import Pin, PWM
 
 class ServoMotor:
     
     def __init__(self):
-        self.pwm = PWM(Pin(1))
         self.freq = 50
+        self.pwm = PWM(Pin(0))
+        self.pwm.freq(self.freq)
+        
         
     # Gira o servo - (1000 é grau 0 e 9000 é grau 180)
     def ativa_servo(self):
-        print(self.freq)
-        for position in range(1000, 9000, self.freq):
+        print("Roda Motor")
+        for position in range(200, 6500, self.freq):
             self.pwm.duty_u16(position)
+            sleep(0.1)
             
+
