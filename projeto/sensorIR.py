ea32c4a57dfcc785dba3a45279db2781fa8bae0e
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-05-25 18:26:22 -0300

    Create sensorIR.py

---
+++ projeto/sensorIR.py
@@ -0,0 +1,19 @@
+from machine import Pin
+import utime
+
+class SensorIR:
+
+    def __init__(self):
+        self.sensor = Pin(15, Pin.IN)
+
+    # Função pra detectar movimento
+    def detecta_movimento(self):
+        if self.sensor.value() == 1:
+            print("Movimento Detectado (IR)!")
+            #self.saida.high()
+            return True
+        else:
+            print("Esperando movimento...(IR)")
+            #self.saida.low()
+            return False
+
