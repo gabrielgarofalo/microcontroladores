c6dbb1a7a59577a9901332389ad970e8678dfc41
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-05-25 18:25:21 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -1,13 +1,17 @@
 from sensorPresenca import SensorPresenca
 from servoMotor import ServoMotor
+from sensorIR import SensorIR
 import utime
 from machine import Pin
 
 servo = ServoMotor()
 sensor_movimento = SensorPresenca()
+ir = SensorIR()
 
 while True:
+    ir.detecta_movimento()
     if sensor_movimento.detecta_movimento():
         servo.ativa_servo()
     utime.sleep(0.2)
     
+
