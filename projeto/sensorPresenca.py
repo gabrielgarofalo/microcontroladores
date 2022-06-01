7af59242ef3e670c3245259c81099ba9638c485f
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-06-01 16:30:32 -0300

    Update sensorPresenca.py

--- projeto/sensorPresenca.py
+++ projeto/sensorPresenca.py
@@ -9,11 +9,11 @@ class SensorPresenca():
     # Função pra detectar movimento
     def detecta_movimento(self):
         if self.entrada.value() == 0:
-            print("Movimento Detectado!")
+            print("Movimento Detectado!(MOV)")
             #self.saida.high()
             return True
         else:
-            print("Esperando movimento...")
+            print("Esperando movimento...(MOV)")
             #self.saida.low()
             return False
             
