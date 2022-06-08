14e975250bd08b0b6953f5d9ff3d6875a35d9c28
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-06-08 17:08:17 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -4,6 +4,8 @@ from sensorIR import SensorIR
 import utime
 from machine import Pin
 
+print("Ola SIstemsa")
+
 servo_ir = ServoMotor(pin=19)
 servo_movimento = ServoMotor(pin=0)
 sensor_movimento = SensorPresenca()
@@ -11,10 +13,9 @@ ir = SensorIR()
 alimentar_agora = Pin(13, Pin.IN)
 
 while True:
+    print(alimentar_agora.value())
     if ir.detecta_movimento():
         servo_ir.ativa_servo()
-    if sensor_movimento.detecta_movimento() or alimentar_agora:
+    if sensor_movimento.detecta_movimento() or alimentar_agora.value() == 1:
         servo_movimento.ativa_servo()
     utime.sleep(0.2)
-    
-
