6ec621d55b16953698dc9f661cb291b1b4c57a96
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2022-06-08 16:33:06 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -8,11 +8,12 @@ servo_ir = ServoMotor(pin=19)
 servo_movimento = ServoMotor(pin=0)
 sensor_movimento = SensorPresenca()
 ir = SensorIR()
+alimentar_agora = Pin(13, Pin.IN)
 
 while True:
     if ir.detecta_movimento():
         servo_ir.ativa_servo()
-    if sensor_movimento.detecta_movimento():
+    if sensor_movimento.detecta_movimento() or alimentar_agora:
         servo_movimento.ativa_servo()
     utime.sleep(0.2)
     
